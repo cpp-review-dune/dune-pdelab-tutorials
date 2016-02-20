3a7a6d37d2fb12b30ce9fef78b44fa1fa9f64d1a
Peter Bastian <peter.bastian@iwr.uni-heidelberg.de>
2016-02-20 15:24:50 +0100

    use new formulation where trial and test space are the same

--- tutorial04/src/wavefem.hh
+++ tutorial04/src/wavefem.hh
@@ -53,7 +53,7 @@ public:
   template<typename EG, typename LFSU, typename X, typename LFSV, typename R>
   void alpha_volume (const EG& eg, const LFSU& lfsu, const X& x, const LFSV& lfsv, R& r) const
   {
-    // select the two components (assume Galerkin scheme U=V)
+    // select the two components (but assume Galerkin scheme U=V)
     auto lfsu0 = lfsu.template child<0>();
     auto lfsu1 = lfsu.template child<1>();
 
@@ -71,9 +71,9 @@ public:
         // evaluate basis functions; Assume basis is the same for both components
         auto& phihat = cache.evaluateFunction(ip.position(),lfsu0.finiteElement().localBasis());
 
-        // evaluate u0
-        RF u0=0.0;
-        for (size_t i=0; i<lfsu0.size(); i++) u0 += x(lfsu0,i)*phihat[i];
+        // evaluate u1
+        RF u1=0.0;
+        for (size_t i=0; i<lfsu0.size(); i++) u1 += x(lfsu1,i)*phihat[i];
 
         // evaluate gradient of shape functions
         auto& gradphihat = cache.evaluateJacobian(ip.position(),lfsu0.finiteElement().localBasis());
@@ -84,16 +84,16 @@ public:
         for (size_t i=0; i<lfsu0.size(); i++)
           S.mv(gradphihat[i][0],gradphi[i][0]);
 
-        // compute gradient of u1
-        Dune::FieldVector<RF,dim> gradu1(0.0);
+        // compute gradient of u0
+        Dune::FieldVector<RF,dim> gradu0(0.0);
         for (size_t i=0; i<lfsu0.size(); i++)
-          gradu1.axpy(x(lfsu1,i),gradphi[i][0]);
+          gradu0.axpy(x(lfsu0,i),gradphi[i][0]);
 
         // integrate both equations
         RF factor = ip.weight() * geo.integrationElement(ip.position());
         for (size_t i=0; i<lfsu0.size(); i++) {
-          r.accumulate(lfsu0,i,c*c*(gradu1*gradphi[i][0])*factor); // first equation
-          r.accumulate(lfsu1,i,-u0*phihat[i]*factor); // second equation
+          r.accumulate(lfsu0,i,c*c*(gradu0*gradphi[i][0])*factor);
+          r.accumulate(lfsu1,i,-u1*phihat[i]*factor);
         }
       }
   }
@@ -131,8 +131,8 @@ public:
         RF factor = ip.weight() * geo.integrationElement(ip.position());
         for (size_t j=0; j<lfsu0.size(); j++)
           for (size_t i=0; i<lfsu0.size(); i++) {
-            mat.accumulate(lfsu0,i,lfsu1,j,c*c*(gradphi[j][0]*gradphi[i][0])*factor); // first eq.
-            mat.accumulate(lfsu1,i,lfsu0,j,-phihat[j]*phihat[i]*factor); // second eq.
+            mat.accumulate(lfsu0,i,lfsu0,j,c*c*(gradphi[j][0]*gradphi[i][0])*factor);
+            mat.accumulate(lfsu1,i,lfsu1,j,-phihat[j]*phihat[i]*factor);
           }
       }
   }
@@ -159,7 +159,7 @@ public:
 };
 
 
-/** a local operator for the mass operator (L_2 integral) for k identical components
+/** a local operator for the temporal operator in the wave equation assuming identical components
  *
  * \f{align*}{
  \int_\Omega uv dx
@@ -167,7 +167,7 @@ public:
  * \tparam FEM      Type of a finite element map
  */
 template<typename FEM>
-class PowerL2
+class WaveL2
   : public Dune::PDELab::FullVolumePattern,
     public Dune::PDELab::LocalOperatorDefaultFlags,
     public Dune::PDELab::InstationaryLocalOperatorDefaultMethods<double>
@@ -190,8 +190,9 @@ public:
   template<typename EG, typename LFSU, typename X, typename LFSV, typename R>
   void alpha_volume (const EG& eg, const LFSU& lfsu, const X& x, const LFSV& lfsv, R& r) const
   {
-    // get first child, assuming PowerGridFunctionSpace
-    auto lfsu0 = lfsu.child(0);
+    // select the two components (assume Galerkin scheme U=V)
+    auto lfsu0 = lfsu.template child<0>();
+    auto lfsu1 = lfsu.template child<1>();
 
     // select quadrature rule
     auto geo = eg.geometry();
@@ -204,21 +205,21 @@ public:
         // evaluate basis functions at first child
         auto& phihat = cache.evaluateFunction(ip.position(),lfsu0.finiteElement().localBasis());
 
+        // evaluate u0
+        RF u0=0.0, u1=0.0;
+        for (size_t i=0; i<lfsu0.size(); i++) {
+          u0 += x(lfsu0,i)*phihat[i];
+          u1 += x(lfsu1,i)*phihat[i];
+        }
+
         // integration factor
         RF factor = ip.weight() * geo.integrationElement(ip.position());
 
-        // loop over all components
-        for (size_t comp=0; comp<LFSU::CHILDREN; comp++)
-          {
-            // evaluate u
-            RF u=0.0;
-            for (size_t i=0; i<lfsu0.size(); i++)
-              u += x(lfsu.child(comp),i)*phihat[i];
-
-            // integrate u*phi_i
-            for (size_t i=0; i<lfsu0.size(); i++)
-              r.accumulate(lfsu.child(comp),i,u*phihat[i]*factor);
-          }
+        // integrate u*phi_i
+        for (size_t i=0; i<lfsu0.size(); i++) {
+          r.accumulate(lfsu0,i,u1*phihat[i]*factor);
+          r.accumulate(lfsu1,i,u0*phihat[i]*factor);
+        }
       }
   }
 
@@ -228,7 +229,8 @@ public:
                         M& mat) const
   {
     // get first child, assuming PowerGridFunctionSpace
-    auto lfsu0 = lfsu.child(0);
+    auto lfsu0 = lfsu.template child<0>();
+    auto lfsu1 = lfsu.template child<1>();
 
     // select quadrature rule
     auto geo = eg.geometry();
@@ -246,9 +248,10 @@ public:
 
         // loop over all components
         for (size_t j=0; j<lfsu0.size(); j++)
-          for (size_t i=0; i<lfsu0.size(); i++)
-            for (size_t comp=0; comp<LFSU::CHILDREN; comp++)
-              mat.accumulate(lfsu.child(comp),i,lfsu.child(comp),j,phihat[j]*phihat[i]*factor);
+          for (size_t i=0; i<lfsu0.size(); i++) {
+            mat.accumulate(lfsu0,i,lfsu1,j,phihat[j]*phihat[i]*factor);
+            mat.accumulate(lfsu1,i,lfsu0,j,phihat[j]*phihat[i]*factor);
+          }
       }
   }
 
