7d6f58eba0d54e0698e80146a619dbec903e0f31
Steffen Müthing <muething@dune-project.org>
2016-03-07 23:39:09 +0100

    [grid-exercise1] Add code for finite volume exercise

---
+++ gridinterface/exercise/solution/fv.hh
@@ -0,0 +1,111 @@
+#ifndef FV_HH
+#define FV_HH
+
+#include <dune/common/fvector.hh>
+#include <dune/grid/common/mcmgmapper.hh>
+
+// Layout class for the mapper
+template<int dim>
+struct FVLayout
+{
+  bool contains(Dune::GeometryType gt)
+  {
+    return gt.dim() == dim;
+  }
+};
+
+// template parameters: GV = gridview type, E = equation parameters
+template<typename rtype, typename GV, typename E>
+class FiniteVolume
+{
+
+public:
+
+  //solution vector
+  using ScalarField = std::vector<rtype>;
+
+private:
+  // mapper for elements (codim=0)
+  using Mapper = Dune::MultipleCodimMultipleGeomTypeMapper<GV,FVLayout>;
+
+  GV grid_view;
+  const E& equation;
+  Mapper mapper;
+  ScalarField update;
+
+public:
+  FiniteVolume(const GV& grid_view_, const E& equation_)
+    : grid_view(grid_view_)
+    , equation(equation_)
+    , mapper(grid_view_)
+    , update(grid_view.size(0))
+  {}
+
+  void update_concentration(ScalarField& concentration, rtype dt)
+  {
+
+    // initialize update vector with 0
+    std::fill(update.begin(),update.end(),0.0);
+
+    for (const auto& cell : elements(grid_view))
+      {
+
+        auto cell_index = mapper.index(cell);
+
+        auto geometry = cell.geometry();
+
+        // cell volume
+        auto volume = geometry.volume();
+
+        // convection term -- boundary integral
+        for (const auto& intersection : intersections(grid_view,cell))
+          {
+
+            auto face_geometry = intersection.geometry();
+
+            // get the global coordinates of the face's center
+            auto center = face_geometry.center();
+
+            // get the outer normal vector at the face center
+            auto normal = intersection.centerUnitOuterNormal();
+
+            // get the velocity vector at the face center
+            auto u = equation.u(center);
+
+            // compute face integral
+            auto flux = (u * normal) * face_geometry.volume() / volume;
+
+            // handle interior face
+            if (intersection.neighbor())
+              {
+                auto outside_index = mapper.index(intersection.outside());
+
+                if (flux > 0.0)           // upwind decision
+                  // outflow
+                  update[cell_index] -= concentration[cell_index]*flux;
+                else
+                  // inflow
+                  update[cell_index] -= concentration[outside_index]*flux;
+              }
+
+            // handle boundary face
+            if (intersection.boundary())
+              {
+                if (flux > 0.0)           // Inflow or outflow?
+                  // outflow
+                  update[cell_index] -= concentration[cell_index]*flux;
+                else
+                  // inflow
+                  update[cell_index] -= equation.c_in(center)*flux;
+              }
+          }
+      }
+
+    // update solution
+    for (std::size_t i = 0; i < concentration.size(); ++i)
+      concentration[i] += update[i] * dt;
+  }
+
+};
+
+#endif
