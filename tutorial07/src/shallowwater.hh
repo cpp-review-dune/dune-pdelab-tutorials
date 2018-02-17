4b96be88b47e58ae34d66336a8100aef1915a7ec
Steffen Müthing <muething@dune-project.org>
2018-02-17 13:33:13 +0100

    [tutorial07] Whitespace cleanups

--- tutorial07/src/shallowwater.hh
+++ tutorial07/src/shallowwater.hh
@@ -47,13 +47,13 @@ public:
 
   //Flux function
   template<typename E, typename X, typename RF>
-  void flux (const E& e, const X& x, 
+  void flux (const E& e, const X& x,
              const Dune::FieldVector<RF,m>& u,
              Dune::FieldMatrix<RF,m,dim>& F) const
   {
     int g = 1;
 
-    F[0][0] = u[1]                          ;
+    F[0][0] = u[1];
     F[1][0] = u[1]*u[1]/u[0] + 0.5*u[0]*u[0];
   }
 
@@ -94,7 +94,7 @@ public:
       alpha_s +=  n_F[k]*u_s[k+1]/u_s[0];
       alpha_n += -n_F[k]*u_n[k+1]/u_n[0];
     }
-    
+
     alpha_s = std::abs(alpha_s) + sqrt(g*u_s[0]);
     alpha_n = std::abs(alpha_n) + sqrt(g*u_n[0]);
 
