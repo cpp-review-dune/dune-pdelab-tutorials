57bc53a70440c6362d638da14d63fbeaecc26315
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-02-08 11:55:12 +0000

    Merge branch 'fixedsize-deprecation-warning' into 'master'
    fixedsize -> fixedSize deprecation warning
    
    See merge request pdelab/dune-pdelab-tutorials!83
    
    (cherry picked from commit 9792bdbb2eabc463326694a4a0cc7ce5676e90fa)
    
    39ba733c fixedsize -> fixedSize deprecation warning

--- tutorial06/exercise/solution/communicate.hh
+++ tutorial06/exercise/solution/communicate.hh
@@ -21,7 +21,7 @@ public:
   }
 
   // Returns true if size per entity of given dim and codim is a constant
-  bool fixedsize (int dim, int codim) const
+  bool fixedSize (int dim, int codim) const
   {
     return true;
   }
